34fe342644facad84ab17df10c93d99034c1be26
中北竜馬 <b1020183@fun.ac.jp>
2024-07-25 20:05:01 +0900

    Minoテストをリファクタリングした

--- spec/mino.rb
+++ spec/mino.rb
@@ -3,59 +3,73 @@ require_relative '../src/mino'
 require_relative '../src/mino_shape'
 
 RSpec.describe Mino do
-  describe 'rotate_right' do
-    it "convex" do
-      mino = Mino.new Mino::Shape::CONVEX
-      mino.rotate_right
-      expect(mino.blocks).to eq [[-1, 1], [-1, 0], [0, 0], [-1, -1]]
-      end
-      
-    it "square" do
-      mino = Mino.new Mino::Shape::SQUARE
-      mino.rotate_right
-      expect(mino.blocks).to eq [[-1, 1], [0, 1], [-1, 0], [0, 0]]
+  describe 'convex' do
+    before do
+      @mino = Mino.new Mino::Shape::CONVEX
     end
 
-    it "lr" do
-      mino = Mino.new Mino::Shape::LR
-      mino.rotate_right
-      expect(mino.blocks).to eq [[-1, 1], [-1, 0], [0, 0], [0, -1]]
+    it 'rotate_right' do
+      @mino.rotate_right
+      expect(@mino.blocks).to eq [[-1, 1], [-1, 0], [0, 0], [-1, -1]]
     end
 
-    it "rl" do
-      mino = Mino.new Mino::Shape::RL
-      mino.rotate_right
-      expect(mino.blocks).to eq [[0, 1], [0, 0], [-1, 0], [-1, -1]]
+    it 'to_buttom' do
+      @mino.to_buttom
+      expect(@mino.blocks).to eq [[0, -1], [1, -1], [1, 0], [2, -1]]
     end
 
-    it "bar" do
-      mino = Mino.new Mino::Shape::BAR
-      mino.rotate_right
-      expect(mino.blocks).to eq [[0, 1], [0, 0], [0, -1], [0, -2]]
+    it 'to_left' do
+      @mino.to_left
+      expect(@mino.blocks).to eq [[-1, -2], [0, -2], [0, -1], [1, -2]]
+    end
+
+    it 'to_right' do
+      @mino.to_right
+      expect(@mino.blocks).to eq [[-1, 0], [0, 0], [0, 1], [1, 0]]
     end
   end
 
-  describe 'to_buttom' do
-    it "convex" do
-      mino = Mino.new Mino::Shape::CONVEX
-      mino.to_buttom
-      expect(mino.blocks).to eq [[0, -1], [1, -1], [1, 0], [2, -1]]
+  describe 'square' do
+    before do
+      @mino = Mino.new Mino::Shape::SQUARE
+    end
+
+    it 'rotate_right' do
+      @mino.rotate_right
+      expect(@mino.blocks).to eq [[-1, 1], [0, 1], [-1, 0], [0, 0]]
     end
   end
 
-  describe 'to_left' do
-    it "convex" do
-      mino = Mino.new Mino::Shape::CONVEX
-      mino.to_left
-      expect(mino.blocks).to eq [[-1, -2], [0, -2], [0, -1], [1, -2]]
+  describe 'lr' do
+    before do
+      @mino = Mino.new Mino::Shape::LR
+    end
+
+    it 'rotate_right' do
+      @mino.rotate_right
+      expect(@mino.blocks).to eq [[-1, 1], [-1, 0], [0, 0], [0, -1]]
     end
   end
 
-  describe 'to_right' do
-    it "convex" do
-      mino = Mino.new Mino::Shape::CONVEX
-      mino.to_right
-      expect(mino.blocks).to eq [[-1, 0], [0, 0], [0, 1], [1, 0]]
+  describe 'rl' do
+    before do
+      @mino = Mino.new Mino::Shape::RL
+    end
+
+    it 'rotate_right' do
+      @mino.rotate_right
+      expect(@mino.blocks).to eq [[0, 1], [0, 0], [-1, 0], [-1, -1]]
+    end
+  end
+
+  describe 'bar' do
+    before do
+      @mino = Mino.new Mino::Shape::BAR
+    end
+
+    it 'rotate_right' do
+      @mino.rotate_right
+      expect(@mino.blocks).to eq [[0, 1], [0, 0], [0, -1], [0, -2]]
     end
   end
 end
